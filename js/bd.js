5b68736c7afc94348abc374857ac74079c3bf88a
Ana Luiza <analuh.eu@gmail.com>
2025-04-24 17:09:03 -0300

    Add files via upload

---
+++ js/bd.js
@@ -0,0 +1,60 @@
+const cidades = [
+    {
+      "titulo": "- Gabriel Eias ",
+      "img": "./img/image.png",
+      "descricao": "“Sorte é ter com quem compartilhar momentos pra não se esquecer, histórias pra gente contar”"
+    },
+    {
+      "titulo": "- Chorão",
+      "img": "./img/img2.jpg",
+      "descricao": "“Felicidade é poder estar com quem você gosta em algum lugar”"
+    },
+    {
+      "titulo": "- Roberto Carlos",
+      "img": "./img/img3.jpg",
+      "descricao": "“Como é grande o meu amor por você”"
+    },
+    {
+      "titulo": "- Cazuza",
+      "img": "./img/img4.jpg",
+      "descricao": "“Daqui até a eternidade”"
+    },
+    {
+      "titulo": " - Vinicius de Moraes & Tom Jobim",
+      "img": "./img/img5.jpg",
+      "descricao": "“Eu sei que vou te amar, por toda a minha vida vou te amar”"
+    },
+    {
+        "titulo": "- Seu Jorge",
+        "img": "./img/img6.jpg",
+        "descricao": "“Felicidade é viver na sua companhia“"
+      },
+      {
+        "titulo": "Legião Urbana",
+        "img": "./img/img7.jpg",
+        "descricao": "“Quem um dia irá dizer que existe razão nas coisas feitas pelo coração?”"
+      },
+      {
+        "titulo": "Barão Vermelho",
+        "img": "./img/img8.jpg",
+        "descricao": "“Por você, eu dançaria tango no teto”"
+      },
+      {
+        "titulo": "Djavan",
+        "img": "./img/img9.jpg",
+        "descricao": "”Por ser exato o amor não cabe em si, por ser encantado o amor revela-se. Por ser amor, invade e fim”"
+      },
+      {
+        "titulo": "- Chorão",
+        "img": "./img/img10.jpg",
+        "descricao": "“Só o amor constrói pontes indestrutíveis”"
+      },
+      {
+        "titulo": "- Chorão",
+        "img": "./img/image.jpg",
+        "descricao": "“Se quem eu amo tem amor por mim, eu sei que ainda estamos muito longe do fim”"
+      },
+
+  ]
+
+  export {cidades}
